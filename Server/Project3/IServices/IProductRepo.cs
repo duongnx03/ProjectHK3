70a70ac7243f6ee1dedc7a79bb730cd9b43eed53
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2024-02-28 16:45:01 +0700

    Add files via upload

---
+++ Server/Project3/IServices/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using Project3.Data;
+using Project3.Models;
+
+namespace Project3.IServices
+{
+    public interface IProductRepo 
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(int id);
+        Task<string> AddProduct(Product product);
+        Task<string> EditProduct(int id, Product product);
+        Task<string> DeleteProduct(int id);
+    }
+} 
