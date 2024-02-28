5d147880456e212f7825ea326ece77f5ddd4f04d
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2024-02-28 18:16:38 +0700

    Add files via upload

---
+++ Server/Project3/IServices/IOrderQuantityRepo.cs
@@ -0,0 +1,10 @@
+﻿using Project3.Models;
+
+namespace Project3.IServices
+{
+    public interface IOrderQuantityRepo
+    {
+        Task<OrderQuantity> AddOrderQuantity(OrderQuantity orderQuantity);
+        Task<List<OrderQuantity>> GetAllOrderQuantities();
+    }
+}
---
+++ Server/Project3/IServices/IOrderWeightRepo.cs
@@ -0,0 +1,10 @@
+﻿using Project3.Models;
+
+namespace Project3.IServices
+{
+    public interface IOrderWeightRepo
+    {
+        Task<OrderWeight> AddOrderWeight(OrderWeight orderWeight);
+        Task<List<OrderWeight>> GetAllOrderWeight();
+    }
+}
