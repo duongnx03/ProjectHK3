c564b45581a7dc0cd4024cbb0126e6d325aaa734
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2024-02-29 13:47:58 +0700

    Add files via upload

---
+++ Server/Project3/Models/Membership.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Runtime.ConstrainedExecution;
+
+namespace Project3.Models
+{
+    public class Membership
+    {
+        [Key]
+        public int MembershipId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Email {  get; set; }
+
+        public int Phone {  get; set; }
+
+        public int Weight { get; set; }
+
+        public DateTime DateReigsterMembership { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+    } 
+}
