735d6647bde63fad83ce7aacc03adee161a322e1
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2024-02-29 13:46:56 +0700

    Add files via upload

---
+++ Server/Project3/IServices/IMembershipRepo.cs
@@ -0,0 +1,16 @@
+﻿using Project3.Models;
+
+namespace Project3.IServices
+{
+    public interface IMembershipRepo
+    {
+        Task<IEnumerable<Membership>> GetAllMemberships();
+        Task<Membership> GetMembershipById(int id);
+        Task<bool> AddMembership(Membership membership);
+        Task<bool> DeleteMembership(int id);
+        Task<bool> RenewMembershipByEmail(string email);
+        Task<bool> OrderMembership(int membershipId, int weightOrdered);
+
+
+    }
+}
